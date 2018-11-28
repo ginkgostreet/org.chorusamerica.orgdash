ff2ca75f127888f3d4a62b70d0251ff00236dcbf
Frank J. Gómez <frank@ginkgostreet.com>
2018-11-27 19:11:08 -0500

    Added boilerplate for new angularjs module.

---
+++ ang/orgdash.ang.php
@@ -0,0 +1,24 @@
+<?php
+// This file declares an Angular module which can be autoloaded
+// in CiviCRM. See also:
+// http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+
+return array (
+  'js' => 
+  array (
+    0 => 'ang/orgdash.js',
+    1 => 'ang/orgdash/*.js',
+    2 => 'ang/orgdash/*/*.js',
+  ),
+  'css' => 
+  array (
+    0 => 'ang/orgdash.css',
+  ),
+  'partials' => 
+  array (
+    0 => 'ang/orgdash',
+  ),
+  'settings' => 
+  array (
+  ),
+);
