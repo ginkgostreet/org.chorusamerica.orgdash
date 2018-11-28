2c3d30e7000c68703dce17d17c854d462fceeae7
Frank J. Gómez <frank@ginkgostreet.com>
2018-11-27 19:39:08 -0500

    Generated boilerplate for AngularJS base page.

---
+++ CRM/Orgdash/Page/Angular.php
@@ -0,0 +1,16 @@
+<?php
+use CRM_Orgdash_ExtensionUtil as E;
+
+class CRM_Orgdash_Page_Angular extends CRM_Core_Page {
+
+  public function run() {
+    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
+    CRM_Utils_System::setTitle(E::ts('Angular'));
+
+    // Example: Assign a variable for use in a template
+    $this->assign('currentTime', date('Y-m-d H:i:s'));
+
+    parent::run();
+  }
+
+}
