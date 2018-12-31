1631464a17743c3cdd858369c4952e93c8e34702
Frank J. Gómez <frank@ginkgostreet.com>
2018-12-31 18:19:03 -0500

    Replaced navigation elements with an implementation of Material's mdNavBar directive.

--- ang/orgdash/controllers/DashCtrl.js
+++ ang/orgdash/controllers/DashCtrl.js
@@ -1,8 +1,14 @@
 (function(angular, $, _) {
 
   angular.module('orgdash')
-    .controller('DashCtrl', function($scope, orgContact) {
+    .controller('DashCtrl', function($scope, $state, orgContact) {
       $scope.name = orgContact.display_name;
+
+      // Initially sets the "active" styling for whichever tab the user entered
+      // on. Once the user interacts with it, the mdNavBar directive keeps this
+      // up to date. Assumes tabs are named like the corresponding states, minus
+      // parent prefixes.
+      $scope.selectedNavItemName = $state.current.name.replace('dash.', '');
     });
 
 })(angular, CRM.$, CRM._);
